2a9e8d9c629c73d2dcc02608b3ae1998bcd8467a
tommy <zzjj0818@gmail.com>
2014-10-28 22:53:22 +0800

    temp commit
    add json data to http request in test_client.py

--- Blizzard/clients.py
+++ Blizzard/clients.py
@@ -7,14 +7,26 @@ app = Flask(__name__)
 
 @app.route("/saveSingleMsg", methods=["POST"])
 def saveSingleMsg():
+    try:
+        para = request.get_json()
+    except:
+        return "required paras not found."
     return "singe message saved."
 
 @app.route("/saveGroupMsg", methods=["POST"])
 def saveGroupMsg():
+    try:
+        para = request.get_json()
+    except:
+        return "required paras not found."
     return "group message saved."
 
 @app.route("/savePicture", methods=["POST"])
 def savePicture():
+    try:
+        para = request.get_json()
+    except:
+        return "required paras not found."
     return "picture saved."
 
 if __name__=="__main__":
--- Blizzard/tests/test_clients.py
+++ Blizzard/tests/test_clients.py
@@ -1,7 +1,9 @@
+# -*- coding:utf-8 -*-
 import os
 import sys
 import unittest
 import httplib
+import json
 
 def initPath():
     project_path = os.path.dirname(
@@ -13,28 +15,37 @@ initPath()
 from Blizzard import clients
 
 class testClients(unittest.TestCase):
-	"""docstring for testClients"""
-	def setUp(self):
-		self._server_ip = "127.0.0.1"
-		self._server_port = 5000
+    """docstring for testClients"""
+    def setUp(self):
+        self._server_ip = "127.0.0.1"
+        self._server_port = 5000
 
-	def test_SaveSingleMsg(self):
-		connection = httplib.HTTPConnection(self._server_ip,self._server_port)
-		connection.request("POST", "/saveSingleMsg")
-		response = connection.getresponse()
-		self.assertEqual(response.read(), "singe message saved.")
+    def test_SaveSingleMsg(self):
+        connection = httplib.HTTPConnection(self._server_ip,self._server_port)
+        headers = {"Content-type":"application/json; charset=UTF-8"}
+        data = {"sender":"Alex", "receiver":"Tom", 
+                "msg":"你好啊,今天天气不错"}
+        connection.request("POST", "/saveSingleMsg",
+            json.JSONEncoder().encode(data), headers)
+        response = connection.getresponse()
+        self.assertEqual(response.read(), "singe message saved.")
 
-	def test_SaveGroupMsg(self):
-		connection = httplib.HTTPConnection(self._server_ip,self._server_port)
-		connection.request("POST", "/saveGroupMsg")
-		response = connection.getresponse()
-		self.assertEqual(response.read(), "group message saved.")
+    def test_SaveGroupMsg(self):
+        connection = httplib.HTTPConnection(self._server_ip,self._server_port)
+        headers = {"Content-type":"application/json; charset=UTF-8"}
+        data = {"sender":"Alex", "group_id":"10011", 
+                "msg":"你好啊,今天天气不错"}
+        connection.request("POST", "/saveGroupMsg")
+        response = connection.getresponse()
+        self.assertEqual(response.read(), "group message saved.")
 
-	def test_SavePicture(self):
-		connection = httplib.HTTPConnection(self._server_ip,self._server_port)
-		connection.request("POST", "/savePicture")
-		response = connection.getresponse()
-		self.assertEqual(response.read(), "picture saved.")
+    def test_SavePicture(self):
+        connection = httplib.HTTPConnection(self._server_ip,self._server_port)
+        headers = {"Content-type":"application/json; charset=UTF-8"}
+        data = {"content":"dsamfeoingsafkg", "format":".jpg"}
+        connection.request("POST", "/savePicture")
+        response = connection.getresponse()
+        self.assertEqual(response.read(), "picture saved.")
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
