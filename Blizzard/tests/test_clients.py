dd6d0129424db49f14c13a6e8fcf924a35250146
alen-alex <zzjj0818@gmail.com>
2014-11-04 10:05:56 +0800

    version alpha 0.3.1
    bug fixes:
    1.fix the unittest not calling sys.exit bug, caused by mis-used "import clients" which
      is not referenced in test_clients.py, and a MessageSaver instance is initiated
      in clients.py who has lead to infinite loops.

--- Blizzard/tests/test_clients.py
+++ Blizzard/tests/test_clients.py
@@ -13,8 +13,6 @@ def initPath():
 
 initPath()
 
-from Blizzard import clients
-
 class testClients(unittest.TestCase):
     """docstring for testClients
        bugs: process will not automatically exit after execution,
@@ -23,51 +21,45 @@ class testClients(unittest.TestCase):
     def setUp(self):
         self._server_ip = "127.0.0.1"
         self._server_port = 5000
+        self.connection = httplib.HTTPConnection(self._server_ip,self._server_port)
         self._message_pool = ["你好啊,今天天气不错","是的啊,有什么打算出游么",
                                 "没有啊，你呢？","我不知道呢，还在家里看电视"]
         self._sender_pool = ["Alex", "Tom"]
 
     def test_SaveSingleMsg(self):
         for i in xrange(100):
-            connection = httplib.HTTPConnection(self._server_ip,self._server_port)
             headers = {"Content-type":"application/json; charset=UTF-8"}
             data = {"sender":self._sender_pool[i%len(self._sender_pool)],
                     "receiver":self._sender_pool[(i+1)%len(self._sender_pool)],
                     "message":[time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()),
                                 self._message_pool[i%len(self._message_pool)]]}
-            connection.request("POST", "/saveSingleMsg",
+            self.connection.request("POST", "/saveSingleMsg",
                 json.JSONEncoder().encode(data), headers)
-            response = connection.getresponse()
-            connection.close()
+            response = self.connection.getresponse()
             self.assertEqual(response.read(), "Singe message saved.")
 
     def test_SaveGroupMsg(self):
-        for i in xrange(1,100):
-            connection = httplib.HTTPConnection(self._server_ip,self._server_port)
+        for i in xrange(100):
             headers = {"Content-type":"application/json; charset=UTF-8"}
             data = {"sender":self._sender_pool[i%len(self._sender_pool)],
                     "group_id":"10011",
                     "message":[time.strftime("%Y-%m-%d %H:%M:%S",time.localtime()),
                                 self._message_pool[i%len(self._message_pool)]]}
-            connection.request("POST", "/saveGroupMsg",
+            self.connection.request("POST", "/saveGroupMsg",
                 json.JSONEncoder().encode(data), headers)
-            response = connection.getresponse()
-            connection.close()
+            response = self.connection.getresponse()
             self.assertEqual(response.read(), "Group message saved.")
 
     def test_SavePicture(self):
-        connection = httplib.HTTPConnection(self._server_ip,self._server_port)
         headers = {"Content-type":"application/json; charset=UTF-8"}
         data = {"content":os.urandom(4096).encode("hex"), "format":".jpg"}
-        connection.request("POST", "/savePicture",
+        self.connection.request("POST", "/savePicture",
             json.JSONEncoder().encode(data), headers)
-        response = connection.getresponse()
-        connection.close()
+        response = self.connection.getresponse()
         self.assertEqual(response.read(), "Picture saved.")
 
     def tearDown(self):
-        del self._message_pool
-        del self._sender_pool
+        self.connection.close()
 
 if __name__ == '__main__':
     unittest.main()
